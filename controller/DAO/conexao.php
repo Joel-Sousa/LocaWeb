27417dc2c9030e4755d5370279f3182a3ecee028
Joel Sousa <softmock@gmail.com>
2021-01-22 10:10:55 -0300

    Update conexao.php

--- controller/DAO/conexao.php
+++ controller/DAO/conexao.php
@@ -5,7 +5,7 @@ abstract class Conexao{
     public static function getInstance(){
         try {
             if(!isset(self::$instance)){
-                self::$instance  = new PDO("mysql: host=localhost;dbname=locaweb;","root","0000");
+                self::$instance  = new PDO("mysql: host=localhost;dbname=locaweb;","root","senha");
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             return self::$instance;
